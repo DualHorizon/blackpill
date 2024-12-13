e0e411eca550d94c2964948e6c49328d2c9837bc
abel <67806187+standard3@users.noreply.github.com>
2024-12-13 18:51:38 +0100

    feat: sample syscall hooking

--- src/hooking/mod.rs
+++ src/hooking/mod.rs
@@ -1,79 +1,100 @@
 //! # Syscall hooking through LSTAR MSR
 
-// use crate::utils::x86::{rdmsr, MSR_LSTAR};
-use kernel::pr_info;
+// use core::arch::asm;
+use core::ffi::c_void;
 
-// pub(crate) fn get_syscall_table() -> Option<*mut *mut u64> {
-//     let system_call: u64 = rdmsr(MSR_LSTAR);
+use kernel::c_str;
+use kernel::fmt;
+use kernel::str::CString;
+use kernel::{bindings, prelude::*, str::CStr};
 
-//     pr_info!("System call address: {:#X}", system_call);
+#[repr(C)]
+pub(crate) struct HListNode {
+    next: *mut HListNode,
+    pprev: *mut *mut HListNode,
+}
 
-//     let ptr = system_call as *const u8;
+#[repr(C)]
+pub(crate) struct ListHead {
+    next: *mut ListHead,
+    prev: *mut ListHead,
+}
 
-//     unsafe {
-//         // loop until first 3 bytes of instructions are found
-//         for offset in 0..500 {
-//             let current = ptr.add(offset);
-//             pr_info!("Offset: {}", offset);
-//             pr_info!("Current: {:#X}", current as u64);
+#[repr(C)]
+pub(crate) struct ArchSpecificInsn {
+    dummy: i32,
+}
+pub(crate) type KProbeOpcode = i32;
 
-//             // Check for the instruction pattern: 0xFF 0x14 0xC5
-//             if *current == 0xFF && *current.add(1) == 0x14 && *current.add(2) == 0xC5 {
-//                 pr_info!("Found syscall table");
-//                 // Compute the address of the syscall table.
-//                 let table_address = 0xFFFFFFFF00000000 | (*(current.add(3) as *const u32) as u64);
-//                 return Some(table_address as *mut *mut u64);
-//             }
-//         }
-//     }
+#[repr(C)]
+pub(crate) struct KProbe {
+    hlist: HListNode,
+    list: ListHead,
+    nmissed: u64,
+    addr: *mut KProbeOpcode,
+    symbol_name: *const i8,
+    offset: u32,
+    pre_handler: Option<unsafe extern "C" fn(*mut c_void) -> i32>,
+    post_handler: Option<unsafe extern "C" fn(*mut c_void) -> i32>,
+    opcode: KProbeOpcode,
+    ainsn: ArchSpecificInsn,
+    flags: u32,
+}
 
-//     None
-// }
+extern "C" {
+    pub(crate) fn register_kprobe(kprobe: *const KProbe) -> core::ffi::c_int;
+    // pub(crate) fn unregister_kprobe(kprobe:*const KProbe);
+}
 
-use crate::utils::x86::MSR_LSTAR;
-use core::arch::asm;
-
-#[allow(unused_assignments)]
-#[allow(unused_mut)]
-pub(crate) fn get_syscall_table() -> Option<*mut *mut u64> {
-    let mut lo: u32 = 0;
-    let mut hi: u32 = 0;
-    let mut system_call: u64;
-
+/// Hook a symbol with a handler.
+/// Arguments:
+/// - `symbol`: The symbol to hook.
+/// - `handler`: The handler to call when the symbol is called.
+/// Returns:
+/// - `Ok(())` if the hook was successful.
+/// - `Err(-1)` if the kmalloc failed.
+/// - `Err(n)` if the register_kprobe failed.
+pub(crate) fn hook(
+    symbol: &CStr,
+    handler: unsafe extern "C" fn(*mut c_void) -> i32,
+) -> Result<(), i32> {
     unsafe {
-        // Read the MSR_LSTAR register to get the system call entry point.
-        asm!(
-            "rdmsr",
-            out("eax") lo,
-            out("edx") hi,
-            in("ecx") MSR_LSTAR,
-        );
+        let kp = bindings::__kmalloc_noprof(core::mem::size_of::<KProbe>(), bindings::GFP_KERNEL)
+            as *mut KProbe;
 
-        system_call = ((hi as u64) << 32) | (lo as u64);
-    }
+        if kp.is_null() {
+            return Err(-1);
+        }
 
-    pr_info!("System call address: {:#X}", system_call);
+        // Convert the syscall name to its symbol counterpart
+        let symbol_name = CString::try_from_fmt(fmt!("__x64_sys_{}", symbol)).unwrap();
 
-    let ptr = system_call as *const u8;
-    let mut found: Option<*mut *mut u64> = None;
+        (*kp).symbol_name = symbol_name.as_char_ptr();
+        (*kp).post_handler = Some(handler);
 
-    unsafe {
-        for offset in 0..500 {
-            let current = ptr.add(offset);
+        let res: i32 = register_kprobe(kp).into();
 
-            pr_info!("Offset: {}", offset);
-            pr_info!("Current: {:#X}", current as u64);
-
-            // Check for the instruction pattern: 0xff 0x14 0xc5
-            if *current == 0xff && *current.add(1) == 0x14 && *current.add(2) == 0xc5 {
-                pr_info!("Found syscall table");
-                // Compute the address of the syscall table.
-                let table_address = 0xffffffff00000000 | (*(current.add(3) as *const u32) as u64);
-                found = Some(table_address as *mut *mut u64);
-                break;
-            }
+        if res != 0 {
+            return Err(res);
+        } else {
+            pr_info!("Hooked\n");
         }
     }
 
-    found
+    Ok(())
+}
+
+/// Hook syscalls with their respective handlers.
+pub(crate) fn hook_syscalls() {
+    pr_info!("Hooking syscalls...\n");
+    let read_hook = hook(c_str!("read"), read_handler);
+
+    if let Err(e) = read_hook {
+        pr_err!("Failed to hook syscall: {}\n", e);
+    }
+}
+
+pub(crate) unsafe extern "C" fn read_handler(_ptr: *mut c_void) -> i32 {
+    pr_info!("Got hooked bro\n");
+    1
 }

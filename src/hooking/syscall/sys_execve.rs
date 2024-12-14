381579e9e75c037621a77e072f508040f2e04cf2
abel <67806187+standard3@users.noreply.github.com>
2024-12-14 17:33:47 +0100

    fix: true return values

--- src/hooking/syscall/sys_execve.rs
+++ src/hooking/syscall/sys_execve.rs
@@ -1,6 +1,7 @@
 use kernel::bindings::pt_regs;
 use kernel::c_str;
 use kernel::prelude::*;
+use kernel::str::CStr;
 
 use super::{hook, KProbe};
 
@@ -13,21 +14,16 @@ pub(crate) fn sys_hook() {
 pub(crate) unsafe extern "C" fn pre_handler(_p: *mut KProbe, _regs: *mut pt_regs) -> i32 {
     pr_info!("execve pre_handler\n");
 
-    pr_info!("regs = {:X}\n", _regs as u64);
-    if _regs == core::ptr::null_mut() {
-        return 1;
-    }
-
     unsafe {
-        let reg = { (*_regs).di };
-        if reg == 0 {
-            pr_info!("execve pre_handler: NULL\n");
-        } else {
-            pr_info!("di: 0x{:X}\n", (*_regs).di);
-        }
+        let p_filename = (*_regs).di;
+        let _p_argv = (*_regs).si;
+        let _p_envp = (*_regs).dx;
+
+        let filename = CStr::from_char_ptr(p_filename as *const i8);
+        pr_info!("filename: {}\n", filename);
     }
 
-    1
+    0
 }
 
 pub(crate) unsafe extern "C" fn post_handler(
@@ -44,5 +40,5 @@ pub(crate) unsafe extern "C" fn post_handler(
             pr_info!("di: 0x{:X}\n", (*_regs).di);
         }
     }
-    1
+    0
 }
